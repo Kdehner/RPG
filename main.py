6e1e09a0469f28cd2449793a23b8ab9ef909d8fd
Kdehner <kevindehner@Emmas-MacBook-Pro.local>
2017-06-19 22:56:18 -0600

    Created basic template for game

---
+++ main.py
@@ -0,0 +1,15 @@
+# RPG is going to be a RogueLike dungeon crawling game.
+
+game = true
+
+# Initalize Game loop
+
+# Create Player Profile
+
+# Create Monsters
+
+# Create Play Area
+
+# Populate Monsters
+
+# Allow
